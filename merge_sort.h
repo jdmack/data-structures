5938766bee1810bdcf8b9cee5ce82914d3fb289c
James Mack <github@wulph.com>
2015-04-23 00:24:40 -0700

    MergeSort works

--- merge_sort.h
+++ merge_sort.h
@@ -1,62 +1,69 @@
-#include <list>
-
+#include <iostream>
+#include <list> 
 template <typename T>
 class MergeSort
 {
     public:
-        std::list<T> sort(std::list<T> list)
+        static std::list<T> * sort(std::list<T> * list)
         {
             // 1 or less elements, return
-            if(list.size() <= 1) {
+            if(list->size() <= 1) {
                 return list;
             }
 
             // Split list in half
-            std::list<T> list1 = std::list<T>();
-            std::list<T> list2 = std::list<T>();
+            std::list<T> * list1 = new std::list<T>();
+            std::list<T> * list2 = new std::list<T>();
                 
-            int size = list.size();
+            int size = list->size();
 
-            for(int i = 0; i < size / 2; ++i) {
-                list2.insert(list.front());
-                list.pop_front();
+            for(int i = 0; i < size; ++i) {
+                if(i < size / 2) {
+                    list1->push_back(list->front());
+                }
+                else {
+                    list2->push_back(list->front());
+                }
+                list->pop_front();
             }
 
             // recurse on each sublist
             list1 = sort(list1);
             list2 = sort(list2);
 
-            list1 = merge(list1, list2);
+            std::list<int> * sorted_list = merge(list1, list2);
+            delete list1;
+            delete list2;
             
-            return list1;
+            return sorted_list;
         }
 
     private:
-        std::list<T> merge(std::list<T> list1, std::list<T> list2)
+        static std::list<T> * merge(std::list<T> * list1, std::list<T>  * list2)
         {
-            std::list<T> sorted_list = std::list<T>();
+            std::list<T> * sorted_list = new std::list<T>();
 
-            while(!list1.empty() && !list2.empty()) {
+            while((!list1->empty()) && (!list2->empty())) {
 
-                // A <= B, add A to sorted_list
-                if(list1.front() <= list2.front()) {
-                    sorted_list.push_back(list1.front());
-                    list1.pop_front();
+                // A < B, add A to sorted_list
+                if(list1->front() < list2->front()) {
+                    sorted_list->push_back(list1->front());
+                    list1->pop_front();
                 }
                 // A > B, add B to sorted_list
                 else {
-                    sorted_list.push_back(list2.front());
-                    list2.pop_front();
+                    sorted_list->push_back(list2->front());
+                    list2->pop_front();
                 }
             }
 
-            while(!list1.empty()) {
-                sorted_list.push_front(list1.front());
-                list1.pop_front();
+            while(!list1->empty()) {
+                sorted_list->push_back(list1->front());
+                list1->pop_front();
             }
-            while(!list2.empty()) {
-                sorted_list.push_back(list2.front());
-                list2.pop_front();
+            while(!list2->empty()) {
+                sorted_list->push_back(list2->front());
+                list2->pop_front();
             }
 
             return sorted_list;
--- test_merge_sort.cpp
+++ test_merge_sort.cpp
@@ -4,19 +4,19 @@
 #include <time.h>
 #include "merge_sort.h"
 
-void print_list(std::list<int> list);
+void print_list(std::list<int> * list);
 
 int main()
 {
     srand(time(NULL)); 
 
-    int num_of_eles = 20;
+    int num_of_eles = 2000000;
 
-    std::list<int> list = std::list<int>();
+    std::list<int> * list = new std::list<int>();
     
     for(int i = 0; i < num_of_eles; ++i) {
         int random_num = rand() % 99;
-        list.push_back(random_num);
+        list->push_back(random_num);
     }
     
     print_list(list);
@@ -27,11 +27,11 @@ int main()
 
 }
 
-void print_list(std::list<int> list)
+void print_list(std::list<int> * list)
 {
     std::cout << "{";
 
-    for(std::list<int>::iterator it = list.begin(); it != list.end(); ++it) {
+    for(std::list<int>::iterator it = list->begin(); it != list->end(); ++it) {
         std::cout << " [" << *it << "]";
     }
     std::cout << " }"  << std::endl;
